b87c6b03daff3706ec3a35a5f4ef11b86459928a
krishkad <krishkadam88@gmail.com>
2024-04-05 17:49:57 +0530

    beautiful iphone mockup

--- src/components/shared/hero.tsx
+++ src/components/shared/hero.tsx
@@ -8,7 +8,7 @@ const Hero = () => {
     return (
         <div className='w-full'>
             <div className="w-full md:h-[calc(100vh-64px)] md:pb-16 flex max-md:flex-col items-center gap-5">
-                <div className="w-full max-h max-md:mt-[80px] max-md:justify-center max-md:items-center md:w-1/2 flex flex-col gap-3 md:gap-5">
+                <div className="w-full max-h max-md:mt-[60px] max-md:justify-center max-md:items-center md:w-1/2 flex flex-col gap-3 md:gap-5">
                     <div className="flex items-center gap-4">
                         <FaVideo className='w-5 h-5 md:w-8 md:h-8' />
                         <span className="text-xl font-bold">kkad <span className="text-primary">Meet</span></span>
@@ -24,14 +24,20 @@ const Hero = () => {
                         </Link>
                     </div>
                 </div>
-                <div className="w-full h-[160vh] md:h-full md:w-1/2">
+                <div className="w-full h-[50vh] md:h-full md:w-1/2">
                     <div className="w-full h-full flex max-md:flex-col justify-center items-center">
-                        <div className="w-full md:w-2/4 h-[80vh]  md:h-full relative">
+                        {/* <div className="max-md:hidden w-full md:w-2/4 h-[80vh]  md:h-full relative">
                             <Image src={'/Rectangle-1.svg'} priority fill sizes='height:100%, width:100%' alt='img' />
                         </div>
-                        <div className="w-full md:w-3/4 h-[80vh] md:h-full relative">
+                        <div className="max-md:hidden w-full md:w-3/4 h-[80vh] md:h-full relative">
                             <Image src={'/Rectangle.svg'} priority fill sizes='height:100%, width:100%' alt='img' />
-                        </div>
+                        </div> */}
+                        <div className=" w-full h-full relative">
+                            <Image src={'/home-mockup.svg'} priority fill sizes='height:100%, width:100%' alt='img' />
+                        </div> 
+                        {/* <div className="relative md:hidden w-full h-full">
+                            <Image src={'/mobile-mockup.svg'} priority fill sizes='height:100%, width:100%' alt='img' />
+                        </div> */}
                     </div>
                 </div>
             </div>
