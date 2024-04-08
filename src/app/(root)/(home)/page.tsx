2114c2d00a7ebf4d5e663da9c5ff52ac45b783b2
krishkad <krishkadam88@gmail.com>
2024-04-08 17:28:45 +0530

    changes

--- src/app/(root)/(home)/page.tsx
+++ src/app/(root)/(home)/page.tsx
@@ -1,5 +1,4 @@
 import Hero from "@/components/shared/hero";
-import PhoneBoard from "@/components/shared/phone-board";
 
 export default function Home() {
   return (
@@ -8,10 +7,9 @@ export default function Home() {
         <Hero />
         <div className="pt-[80px] pb-10">
           <div className="flex flex-col jusitfy-center items-center gap-1 pb-10">
-            <span className="text-3xl font-bold text-center">Lorem, ipsum dolor.</span>
-            <span className="text-sm md:text-xl text-center">Lorem ipsum dolor sit amet consectetur..</span>
+            <span className="text-3xl font-bold text-center">15 Million Fans Love</span>
+            <span className="text-sm text-center lg:w-1/2">Discover Kkad Meet: the cherished video calling choice of over 15 million users globally. With pristine audio and video quality, Kkad Meet effortlessly connects friends, families, and colleagues. Join the millions who love its user-friendly interface and reliable performance.</span>
           </div>
-          <PhoneBoard />
         </div>
       </div>
     </main>
