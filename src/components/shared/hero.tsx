5a77ec09f48df199e5ff2fbc93c8c0e26df392ca
krishkad <krishkadam88@gmail.com>
2024-04-12 23:02:47 +0530

    improve

--- src/components/shared/hero.tsx
+++ src/components/shared/hero.tsx
@@ -1,10 +1,7 @@
 import React from 'react'
 import { Button } from '../ui/button'
-import { FaVideo } from "react-icons/fa";
-
 import Link from 'next/link';
 import HeroImage from './hero-image';
-import Image from 'next/image';
 
 const Hero = () => {
     return (
@@ -13,10 +10,6 @@ const Hero = () => {
 
                 <div className="w-full flex flex-col items-center ">
                     <div className="flex w-full xl:w-1/2 mt-16 flex-col items-center justify-center gap-3">
-                        <div className="flex items-center gap-4">
-                            <FaVideo className='w-5 h-5 md:w-8 md:h-8' />
-                            <span className="text-xl font-bold">kkad <span className="text-primary">Meet</span></span>
-                        </div>
                         <span className="text-4xl md:text-6xl font-bold text-center">Video calls with <br />anyone, anywhere</span>
                         <span className="text-sm font-medium text-muted-foreground text-center">Stay connected and collaborate with friends, family, and colleagues no matter where you are.</span>
                         <div className="flex items-center gap-5">
