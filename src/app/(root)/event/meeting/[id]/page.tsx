86f200c8422cdffcbf67da33697e48d06df48a23
krishkad <krishkadam88@gmail.com>
2024-04-29 14:10:49 +0530

    video setup & video room remaining

--- src/app/(root)/event/meeting/[id]/page.tsx
+++ src/app/(root)/event/meeting/[id]/page.tsx
@@ -1,8 +1,14 @@
-import React from 'react'
+"use client";
+import React, { useState } from 'react';
+import VideoRoom from '@/components/shared/video-room';
+import VideoSetup from '@/components/shared/video-setup';
 
 const MeetingPage = ({ params }: { params: { id: string } }) => {
+  const [isSetupComplete, setIsSetupComplete] = useState(false);
   return (
-    <div className='w-full '>MeetingPage: {params.id}</div>
+    <div className='w-full '>
+      {isSetupComplete ? <VideoSetup /> : <VideoRoom />};
+    </div>
   )
 }
 
---
+++ src/components/shared/video-setup.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const VideoSetup = () => {
+    return (
+        <div>VideoSetup</div>
+    )
+}
+
+export default VideoSetup
