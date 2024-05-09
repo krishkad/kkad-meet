73f8e2a97415e0573e5a2945bba3e6ecb5e52deb
krishkad <krishkadam88@gmail.com>
2024-05-09 10:54:23 +0530

    setup button

--- src/components/shared/meeting-setup.tsx
+++ src/components/shared/meeting-setup.tsx
@@ -4,23 +4,37 @@ import React, { useEffect, useState } from 'react';
 import { Checkbox } from '../ui/checkbox';
 import { Label } from '../ui/label';
 import { Button } from '../ui/button';
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
+import {
+    Card,
+    CardContent
+} from "@/components/ui/card"
+import { MicOff, Video, VideoOff } from 'lucide-react';
+import { Mic } from 'lucide-react';
 
 const MeetingSetup = ({ setIsSetupComplete }: { setIsSetupComplete: (value: boolean) => void }) => {
-    const [toggleCamMicOn, setToggleCamMicOn] = useState(false);
+    const [video, setVideo] = useState(true);
+    const [mic, setMic] = useState(true);
     const call = useCall();
 
     if (!call) {
         throw new Error("useCall should be used in StreamCall");
     }
+
     useEffect(() => {
-        if (toggleCamMicOn) {
-            call?.microphone.disable();
-            call?.camera.disable();
+        if (video) {
+            call.camera.disable();
         } else {
-            call?.microphone.enable();
-            call?.camera.enable();
+            call.camera.enable();
         }
-    }, [toggleCamMicOn, call?.microphone, call?.camera]);
+        if (mic) {
+            call.microphone.disable();
+        } else {
+            call.microphone.enable();
+        }
+
+    }, [video, mic, call?.microphone, call?.camera])
+
 
     return (
         <div className="max-w-wrapper overflow-hidden">
@@ -29,15 +43,40 @@ const MeetingSetup = ({ setIsSetupComplete }: { setIsSetupComplete: (value: bool
                 <div className="relative">
                     <VideoPreview mirror={false} />
                 </div>
-                <div className="w-full">
+                <div className="w-full flex justify-center">
                     <div className="flex items-center space-x-2">
-                        <Checkbox id="camera-mic" name='camera-mic' onClick={(e: any) => setToggleCamMicOn(!toggleCamMicOn)} />
-                        <Label
-                            htmlFor="camera-mic"
-                            className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-                        >
-                            Turn On/Off Camera and Mic
-                        </Label>
+                        <Card className='border-none shadow-none'>
+                            <CardContent className="grid gap-6 border-none p-0">
+                                <RadioGroup defaultValue="video" name='setup-specs' className="grid grid-cols-2 gap-4">
+                                    <div>
+                                        <RadioGroupItem value="video" id="video" className="peer sr-only" onClick={() => setVideo(!video)} checked={true} />
+                                        <Label
+                                            htmlFor="video"
+                                            className={`aspect-square flex flex-col items-center justify-between rounded-full border-2 border-muted bg-popover p-4  ${!video && "bg-primary text-white border-primary"} font-semibold`}
+                                        >
+
+                                            {!video ? <VideoOff /> : <Video />}
+                                        </Label>
+                                    </div>
+                                    <div>
+                                        <RadioGroupItem
+                                            value="mic"
+                                            id="mic"
+                                            className="peer sr-only"
+                                            onClick={() => setMic(!mic)}
+                                            checked={true}
+                                        />
+                                        <Label
+                                            htmlFor="mic"
+                                            className={`aspect-square flex flex-col items-center justify-between rounded-full border-2 border-muted bg-popover p-4  ${!mic && "bg-primary text-white border-primary"} font-semibold`}
+                                        >
+
+                                            {!mic ? <MicOff /> : <Mic />}
+                                        </Label>
+                                    </div>
+                                </RadioGroup>
+                            </CardContent>
+                        </Card>
                     </div>
                 </div>
                 <div className="w-full flex justify-center items-center mt-10">
